89772b14af8400c2c9a3f28fad32e8117a6dddbb
Silverio del Rosario <plussilver1@gmail.com>
2020-04-04 13:29:52 -0600

    Codigo video chat Laravel Vuejs

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use \Pusher\Pusher;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function authenticate(Request $request) {
+        $socketId = $request->socket_id;
+        $channelName = $request->channel_name;
+
+        $pusher = new Pusher('fe9765dd7880e11b96ef', 'a47bfadab8fc57efbd5d', '738075', [
+            'cluster' => 'us2',
+            'encrypted' => true
+        ]);
+
+        $presence_data = ['name' => auth()->user()->name];
+        $key = $pusher->presence_auth($channelName, $socketId, auth()->id(), $presence_data);
+
+        return response($key);
+    }
+}
